055e27955ccb72873955779744027ef390bc039a
D4rkne <128006468+D4rkne@users.noreply.github.com>
2024-04-01 22:17:47 -0300

    Add files via upload

---
+++ vs codee/main.js
@@ -0,0 +1,7 @@
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;<botoes.length,ii++){
+    botoes[i].onclick = function(){
+        botoes[i].classlist.add("ativo")       
+    console.log();
+}
